5fc383a12b82f58be2df0b7f7ae34e94875195f8
“dicristea” <dicristea111@gmail.com>
2022-08-14 16:02:49 -0700

    popup form added

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let myLibrary = [];
 
+
+const readButton = document.querySelector('.read-button');
+
+
+
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -12,9 +17,35 @@ function Book(title, author, pages, isRead) {
     };
 };
 
-const book = new Book('Harry Potter', 'Lady', 700, true);
-console.log(book.info()); 
+function openForm() {
+    document.getElementById("bookFormPopup").style.display = "block";
+};
 
-function addBookToLibrary () {
-    // do stuff here
+function closeForm() {
+    document.getElementById("bookFormPopup").style.display = "none";
+};
+
+function removeBook() {
+    // use data attribute linked to each card somehow and delete indexed book
+    updateDisplay();
+};
+
+function changeReadStatus() {
+    //changes data index book read status to true or false;
+
+    if (readButton.textContent === 'Read') {
+        readButton.textContent = 'Unread';
+    } else {
+        readButton.textContent = 'Read'
+    };
+};
+
+function addBookToLibrary() {
+    const book = new Book('Harry Potter', 'Lady', 700, true);
+    console.log(book.info());
+    
+};
+
+function updateDisplay() {
+        // loop thru array and make cards for every object in array
 };
